bbefbcb9fa5493cdb8304db5a0f6934a29b6d57d
AdityaSah2030 <adityasah2030@gmail.com>
2025-08-13 18:57:02 +0530

    update chai business

--- 06_chai_business/main.py
+++ 06_chai_business/main.py
@@ -1,7 +1,7 @@
-# import recipes.flavors
+import recipes.flavors
 
-# print(recipes.flavors.ginger_chai())
+print(recipes.flavors.ginger_chai())
 
-# from recipes.flavors import elachai_chai, ginger_chai
+from recipes.flavors import elachai_chai, ginger_chai
 
-# print(ginger_chai())
+print(ginger_chai())
